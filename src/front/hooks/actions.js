3ceebebd4494ec4121e15d8ba845f4adf8516ebf
Byron Rolle JR <dareal4our@gmail.com>
2025-05-28 23:43:28 +0000

    0.1

---
+++ src/front/hooks/actions.js
@@ -0,0 +1,23 @@
+// import React from'react';
+
+// const getToken = async (dispatch, payload) => {
+//     let response = await fetch("https://accounts.spotify.com/api/token" 
+//         ,{
+        
+//         method: "POST",
+//         type: "application/x-www-form-urlencoded",
+//         body: 
+//         "grant_type": client_credentials
+//         "client_id" : your-client-id
+//         "client_secret" :your-client-secret"
+        
+//     })
+//     console.log(getToken)
+}
+
+    // curl -X POST "https://accounts.spotify.com/api/token" \
+    //  -H "Content-Type: application/x-www-form-urlencoded" \
+    //  -d "grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret"
+
+    // Client ID be2d4973f1a84fa0a986a6e1bc360bca
+    // Client Secret 643f2486bb464e16b26c29adf54309e4
